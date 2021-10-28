699a3eb61cce2ecc69ed5e6750faefbfacefa1a7
Shoyayeb <sa.fine@Outlook.com>
2021-10-28 20:42:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4f4qc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("carMechanic");
+    const servicesCollection = database.collection("services");
+
+    // GET API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // GET SINGLE SERVICE
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("getting ", id);
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+
+    // POST API
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      console.log(service);
+      const result = await servicesCollection.insertOne(service);
+      console.log(result);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running genius server");
+});
+
+app.listen(port, () => {
+  console.log("Running server on", port);
+});
